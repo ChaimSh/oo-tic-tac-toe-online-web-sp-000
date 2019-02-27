8334bddfc83ad928f292a9eb01a720ca7e70515c
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-02-27 01:06:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,8 @@ class TicTacToe
    [2,4,6]
  ]
 
- def display_board(board)
+ def display_board=(board)
+   @board = board
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
  puts   "-----------"
    puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -23,5 +24,8 @@ class TicTacToe
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
 
+def display_board
+  @board
+end
 
 end
